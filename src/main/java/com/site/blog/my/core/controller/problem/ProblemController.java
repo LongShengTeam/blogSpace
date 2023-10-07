1c6a81a98174e32584f49eef059aa84d3a84976b
hanjing_xl@163.com <hanjing_xl@163.com>
2023-10-07 09:54:42 +0800

    刷题

--- src/main/java/com/site/blog/my/core/controller/problem/ProblemController.java
+++ src/main/java/com/site/blog/my/core/controller/problem/ProblemController.java
@@ -30,8 +30,8 @@ public class ProblemController {
     private LinkService linkService;
     @Resource
     HttpServletRequest request;
-    private static HashMap<String, List<Question>> moniHashMap=new HashMap<>();
-    private static HashMap<String, List<Question>> errorMap=new HashMap<String, List<Question>>();
+    private static HashMap<String, List<Question>> moniHashMap = new HashMap<>();
+    private static HashMap<String, List<Question>> errorMap = new HashMap<String, List<Question>>();
 
     @GetMapping("/next")
     public String next(@RequestParam("id") Integer id) {
@@ -52,11 +52,11 @@ public class ProblemController {
     }
 
     @GetMapping("/next3")
-    public String next(@RequestParam("id") Integer id,@RequestParam("userName") String userName,
+    public String next(@RequestParam("id") Integer id, @RequestParam("userName") String userName,
                        @RequestParam("type") Integer type) {
-        log.info("id-->{}",id);
-        log.info("userName-->{}",userName);
-        log.info("type-->{}",type);
+        log.info("id-->{}", id);
+        log.info("userName-->{}", userName);
+        log.info("type-->{}", type);
         log.info("刷题日志");
         request.setAttribute("pageName", "友情链接");
         HashMap<Integer, Question> questionHashMap = fenxiData();
@@ -65,7 +65,7 @@ public class ProblemController {
 
         request.setAttribute("userName", userName);
         request.setAttribute("type", type);
-        switch (type){
+        switch (type) {
 //              <option value="1" th:selected="${type == 1}"  autocomplete="off">顺序</option>
             case 1:
                 request.setAttribute("question", questionHashMap.get(id));
@@ -73,47 +73,49 @@ public class ProblemController {
                 break;
 //            <option value="2" th:selected="${type == 2}"  autocomplete="off">随机</option>
             case 2:
-                 id = new Random().nextInt(2311);
+                id = new Random().nextInt(2311);
                 log.info("---->{}", questionHashMap.get(id));
                 request.setAttribute("question", questionHashMap.get(id));
                 break;
 //            <option value="3" th:selected="${type == 3}"  autocomplete="off">模拟100题</option>
             case 3:
-                List<Question> questionList = moniHashMap.get("userName");
-                List<Question> noDonecollect=null;
+                List<Question> questionList = moniHashMap.get(userName);
+                List<Question> noDonecollect = null;
 
-                if(questionList!=null){
-                    Boolean done=true;
-                    noDonecollect= questionList.parallelStream().filter(row -> !row.getDone()).collect(Collectors.toList());
-                    if(noDonecollect.size()==0){
-                        questionList=new ArrayList<>();
+                if (questionList != null) {
+                    Boolean done = true;
+                    noDonecollect = questionList.parallelStream().filter(row -> !row.getDone()).collect(Collectors.toList());
+                    if (noDonecollect.size() == 0) {
+                        questionList = new ArrayList<>();
                         ArrayList<Integer> objects = new ArrayList<>();
-                        while (questionList.size()<=100){
+                        while (questionList.size() < 100) {
                             id = new Random().nextInt(2311);
-                            if(!objects.contains(id)){
+                            if (!objects.contains(id)) {
                                 objects.add(id);
                                 questionList.add(questionHashMap.get(id));
                             }
 
                         }
+                        moniHashMap.put(userName, questionList);
                     }
-                }else{
+                } else {
 
-                    questionList=new ArrayList<>();
+                    questionList = new ArrayList<>();
                     ArrayList<Integer> objects = new ArrayList<>();
-                    while (questionList.size()<=100){
+                    while (questionList.size() < 100) {
                         id = new Random().nextInt(2311);
-                        if(!objects.contains(id)){
+                        if (!objects.contains(id)) {
                             objects.add(id);
                             questionList.add(questionHashMap.get(id));
                         }
 
                     }
+                    moniHashMap.put(userName, questionList);
 
 
                 }
-                questionList.forEach(row->{
-                    if(!row.getDone()){
+                questionList.forEach(row -> {
+                    if (!row.getDone()) {
                         request.setAttribute("question", row);
                         log.info("---->{}", row);
                     }
@@ -123,9 +125,9 @@ public class ProblemController {
 //            <option value="4" th:selected="${type == 4}"  autocomplete="off">错题</option>
             case 4:
                 List<Question> errorQuestionList = errorMap.get("userName");
-                if(errorQuestionList!=null){
+                if (errorQuestionList != null) {
                     request.setAttribute("question", errorQuestionList.get(0));
-                }else{
+                } else {
                     request.setAttribute("question", questionHashMap.get(id));
                 }
                 break;
@@ -141,90 +143,123 @@ public class ProblemController {
     }
 
 
-
-    @PostMapping("/next2")
+    /**
+     * 回答问题
+     *
+     * @param questResultDTO
+     * @return
+     */
+    @PostMapping("/answer")
     @ResponseBody
-    public String next2(@RequestBody QuestResultDTO questResultDTO) {
-        log.info("questResultDTO-->{}",questResultDTO);
+    public String answer(@RequestBody QuestResultDTO questResultDTO) {
+        log.info("questResultDTO-->{}", questResultDTO);
         HashMap<Integer, Question> questionHashMap = fenxiData();
         Question question = questionHashMap.get(questResultDTO.getId());
         List<Question> questions = errorMap.get(questResultDTO.getUserName());
-        AtomicReference<String> result=null;
-        if(questions==null){
-            questions=new ArrayList<Question>();
-            errorMap.put(questResultDTO.getUserName(),questions);
+        AtomicReference<String> result = null;
+        if (questions == null) {
+            questions = new ArrayList<Question>();
+            errorMap.put(questResultDTO.getUserName(), questions);
         }
-        switch (questResultDTO.getType()){
+        switch (questResultDTO.getType()) {
             case 1:
                 result = new AtomicReference<>("答对:" + question.getAnswerList());
                 List<Question> finalQuestions = questions;
                 AtomicReference<String> finalResult = result;
-                question.getAnswerList().forEach(k->{
-                    if(!questResultDTO.getAnswerList().contains(k)){
-                        finalQuestions.add(question);
-                        finalResult.set("答错:正确答案是" + question.getAnswerList().toString());
+                Question finalQuestion = question;
+                question.getAnswerList().forEach(k -> {
+                    if (!questResultDTO.getAnswerList().contains(k)) {
+                        finalQuestions.add(finalQuestion);
+                        finalResult.set("答错:正确答案是" + finalQuestion.getAnswerList().toString());
                     }
                 });
                 AtomicReference<String> finalResult1 = result;
-                questResultDTO.getAnswerList().forEach(k->{
-                    if(!question.getAnswerList().contains(k)){
-                        finalQuestions.add(question);
-                        finalResult1.set("答错:正确答案是" + question.getAnswerList().toString());
+                questResultDTO.getAnswerList().forEach(k -> {
+                    if (!finalQuestion.getAnswerList().contains(k)) {
+                        finalQuestions.add(finalQuestion);
+                        finalResult1.set("答错:正确答案是" + finalQuestion.getAnswerList().toString());
                     }
                 });
                 break;
             case 2:
-                 result = new AtomicReference<>("答对:" + question.getAnswerList());
+                result = new AtomicReference<>("答对:" + question.getAnswerList());
                 List<Question> finalQuestions2 = questions;
                 AtomicReference<String> finalResult2 = result;
-                question.getAnswerList().forEach(k->{
-                    if(!questResultDTO.getAnswerList().contains(k)){
-                        finalQuestions2.add(question);
-                        finalResult2.set("答错:正确答案是" + question.getAnswerList().toString());
+                Question finalQuestion1 = question;
+                question.getAnswerList().forEach(k -> {
+                    if (!questResultDTO.getAnswerList().contains(k)) {
+                        finalQuestions2.add(finalQuestion1);
+                        finalResult2.set("答错:正确答案是" + finalQuestion1.getAnswerList().toString());
                     }
                 });
-                questResultDTO.getAnswerList().forEach(k->{
-                    if(!question.getAnswerList().contains(k)){
-                        finalQuestions2.add(question);
-                        finalResult2.set("答错:正确答案是" + question.getAnswerList().toString());
+                questResultDTO.getAnswerList().forEach(k -> {
+                    if (!finalQuestion1.getAnswerList().contains(k)) {
+                        finalQuestions2.add(finalQuestion1);
+                        finalResult2.set("答错:正确答案是" + finalQuestion1.getAnswerList().toString());
                     }
                 });
 
                 break;
             case 3:
-                result = new AtomicReference<>("答对:" + question.getAnswerList());
+                //模拟答题
+                List<Question> moniQuestionList = moniHashMap.get(questResultDTO.getUserName());
+                Question finalQuestion4 = question;
+                List<Question> collect = moniQuestionList.parallelStream().filter(row -> row.getId().equals(finalQuestion4.getId())).collect(Collectors.toList());
+                if(collect!=null && collect.size()>0){
+                    question=collect.get(0);
+                }else{
+                    log.error("question--->{}",question);
+                    log.error("moniQuestionList--->{}",moniQuestionList);
+                }
+                String s = "答对:" + question.getAnswerList();
+                result = new AtomicReference<>(s);
+
+
                 List<Question> finalQuestions3 = questions;
                 AtomicReference<String> finalResult3 = result;
-                question.getAnswerList().forEach(k->{
-                    if(!questResultDTO.getAnswerList().contains(k)){
-                        finalQuestions3.add(question);
-                        finalResult3.set("答错:正确答案是" + question.getAnswerList().toString());
+                question.setError(false);
+                question.setDone(true);
+                Question finalQuestion2 = question;
+                question.getAnswerList().forEach(k -> {
+                    if (!questResultDTO.getAnswerList().contains(k)) {
+                        finalQuestion2.setError(true);
+                        finalQuestions3.add(finalQuestion2);
+                        finalResult3.set("答错:正确答案是" + finalQuestion2.getAnswerList().toString());
                     }
                 });
 
-                questResultDTO.getAnswerList().forEach(k->{
-                    if(!question.getAnswerList().contains(k)){
-                        finalQuestions3.add(question);
-                        finalResult3.set("答错:正确答案是" + question.getAnswerList().toString());
+                questResultDTO.getAnswerList().forEach(k -> {
+                    if (!finalQuestion2.getAnswerList().contains(k)) {
+                        finalQuestion2.setError(true);
+                        finalQuestions3.add(finalQuestion2);
+                        finalResult3.set("答错:正确答案是" + finalQuestion2.getAnswerList().toString());
                     }
                 });
+                s += "--->总共" + moniQuestionList.size() + "个,";
+                long errorCount = moniQuestionList.parallelStream().filter(row -> row.getError() != null && row.getError()).count();
+                s += "--->错了" + errorCount + "个,";
+                long rightCount = moniQuestionList.parallelStream().filter(row -> row.getError() != null && !row.getError()).count();
+                s += "--->对了" + rightCount + "个!";
+                finalResult3.set(finalResult3.get() + s);
                 break;
             case 4:
                 result = new AtomicReference<>("答对:" + question.getAnswerList());
                 List<Question> finalQuestions4 = questions;
                 AtomicReference<String> finalResult4 = result;
-                question.getAnswerList().forEach(k->{
-                    if(!questResultDTO.getAnswerList().contains(k)){
-                        finalQuestions4.add(question);
-                        finalResult4.set("答错:正确答案是" + question.getAnswerList().toString());
+                Question finalQuestion3 = question;
+                question.getAnswerList().forEach(k -> {
+                    if (!questResultDTO.getAnswerList().contains(k)) {
+                        finalQuestions4.add(finalQuestion3);
+                        finalResult4.set("答错:正确答案是" + finalQuestion3.getAnswerList().toString());
                     }
                 });
-                questResultDTO.getAnswerList().forEach(k->{
-                    if(!question.getAnswerList().contains(k)){
-                        finalQuestions4.add(question);
-                        finalResult4.set("答错:正确答案是" + question.getAnswerList().toString());
+                questResultDTO.getAnswerList().forEach(k -> {
+                    if (!finalQuestion3.getAnswerList().contains(k)) {
+                        finalQuestions4.add(finalQuestion3);
+                        finalResult4.set("答错:正确答案是" + finalQuestion3.getAnswerList().toString());
                     }
                 });
+
                 break;
 
 
@@ -234,7 +269,6 @@ public class ProblemController {
         request.setAttribute("configurations", configService.getAllConfigs());
         return result.get();
     }
-
 
 
     private HashMap<Integer, Question> fenxiData() {
@@ -301,8 +335,6 @@ public class ProblemController {
         });
         return questionHashMap;
     }
-
-
 
 
 }
